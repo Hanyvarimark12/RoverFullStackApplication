5bddc204204309c30804da0e325ad20801a269e5
hanyvari.mark <hanyvarimark@stud.uni-obuda.hu>
2023-11-14 17:45:13 +0100

    RoverBuilderLogic added

---
+++ DEAIC6_HFT_2023241.Tester/RoverLogicTester.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+using System;
+using DEAIC6_HFT_2023241.Logic;
+
+namespace DEAIC6_HFT_2023241.Tester
+{
+    [TestFixture]
+    public class RoverLogicTester
+    {
+        
+    }
+}
