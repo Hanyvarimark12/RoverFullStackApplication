51a5577e4a2e7e78c0dad1320faf5458b87c6452
hanyvari.mark <hanyvarimark@stud.uni-obuda.hu>
2023-11-14 17:59:16 +0100

    IRepository modified

--- DEAIC6_HFT_2023241.Repository/IRepository.cs
+++ DEAIC6_HFT_2023241.Repository/IRepository.cs
@@ -2,7 +2,7 @@
 
 namespace DEAIC6_HFT_2023241.Repository
 {
-    internal interface IRepository<T> where T : class
+    public interface IRepository<T> where T : class
     {
         IQueryable<T> ReadAll();
         T Read(int id);
