cd59e842cfcf22afb9e802dc54941b53b3a2e479
hanyvari.mark <hanyvarimark@stud.uni-obuda.hu>
2023-12-01 10:21:03 +0100

    VisitedPlaces model modified

--- DEAIC6_HFT_2023241.Models/VisitedPlaces.cs
+++ DEAIC6_HFT_2023241.Models/VisitedPlaces.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace DEAIC6_HFT_2023241.Models
@@ -14,13 +15,20 @@ namespace DEAIC6_HFT_2023241.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int PlaceId { get; set; }
         [StringLength(60)]
+        [Required]
         public string PlanetName { get; set; }
         [StringLength(60)]
         public string PlanetType { get; set;}
         [Required]
         public double Distance { get; set; }
-
+        [JsonIgnore]
+        [NotMapped]
         public virtual ICollection<RoverBuilder> RoverBuilders { get; set; }
+
+        public VisitedPlaces()
+        {
+            RoverBuilders = new HashSet<RoverBuilder>();
+        }
 
         public VisitedPlaces(string line)
         {
@@ -29,6 +37,7 @@ namespace DEAIC6_HFT_2023241.Models
             PlanetName = visitedPlacesLine[1];
             PlanetType = visitedPlacesLine[2];
             Distance = double.Parse(visitedPlacesLine[3]);
+            RoverBuilders = new HashSet<RoverBuilder>();
         }
 
         //PlaceId#PlanetName#PlanetType#Distance
