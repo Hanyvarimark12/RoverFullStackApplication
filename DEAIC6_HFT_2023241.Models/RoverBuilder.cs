2feada01d8f911db4b962f63a0d365130ccdf521
hanyvari.mark <hanyvarimark@stud.uni-obuda.hu>
2023-12-01 10:20:42 +0100

    RoverBuilder model modified

--- DEAIC6_HFT_2023241.Models/RoverBuilder.cs
+++ DEAIC6_HFT_2023241.Models/RoverBuilder.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace DEAIC6_HFT_2023241.Models
@@ -15,14 +16,26 @@ namespace DEAIC6_HFT_2023241.Models
         public int BuilderId { get; set; }
         [StringLength(60)]
         public string BuilderName { get; set; }
+        [Required]
+        public int VisitedPlaceId { get; set; }
+        [NotMapped]
+        public virtual VisitedPlaces VisitedPlaces { get; set; }
+        [JsonIgnore]
+        [NotMapped]
+        public virtual ICollection<Rover> Rovers { get; set; }
 
-        public virtual ICollection<VisitedPlaces> VisitedPlaces { get; set; }
+        public RoverBuilder()
+        {
+            Rovers = new HashSet<Rover>();
+        }
 
         public RoverBuilder(string line)
         {
             string[] builderLine = line.Split('#');
             BuilderId = int.Parse(builderLine[0]);
             BuilderName = builderLine[1];
+            VisitedPlaceId = int.Parse(builderLine[2]);
+            Rovers = new HashSet<Rover>();
         }
 
         //BuilderId#BuilderName
