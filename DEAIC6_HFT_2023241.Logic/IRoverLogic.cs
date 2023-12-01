3d7f197fb0336723e534502029abd9c536e5b71b
hanyvari.mark <hanyvarimark@stud.uni-obuda.hu>
2023-12-01 10:42:08 +0100

    IRoverLogic added

---
+++ DEAIC6_HFT_2023241.Logic/IRoverLogic.cs
@@ -0,0 +1,16 @@
+﻿using DEAIC6_HFT_2023241.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DEAIC6_HFT_2023241.Logic
+{
+    public interface IRoverLogic
+    {
+        void Create(Rover element);
+        void Delete(int id);
+        IEnumerable<RoverNumber> BuilderRovers();
+        Rover Read(int id);
+        IQueryable<Rover> ReadAll();
+        void Update(Rover Element);
+    }
+}
