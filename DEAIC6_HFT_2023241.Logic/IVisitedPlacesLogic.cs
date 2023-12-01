f7813a5240f9b3afdf604d6f2aad23f9c412e049
hanyvari.mark <hanyvarimark@stud.uni-obuda.hu>
2023-12-01 10:42:21 +0100

    IVisitedPlacesLogic added

---
+++ DEAIC6_HFT_2023241.Logic/IVisitedPlacesLogic.cs
@@ -0,0 +1,17 @@
+﻿using DEAIC6_HFT_2023241.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DEAIC6_HFT_2023241.Logic
+{
+    public interface IVisitedPlacesLogic
+    {
+        void Create(VisitedPlaces element);
+        void Delete(int id);
+        RoverNumberByPlanet MostRoverNumber();
+        VisitedPlaces Read(int id);
+        IQueryable<VisitedPlaces> ReadAll();
+        void Update(VisitedPlaces Element);
+        IEnumerable<RoverTraveled> VisitedByBuilder();
+    }
+}
