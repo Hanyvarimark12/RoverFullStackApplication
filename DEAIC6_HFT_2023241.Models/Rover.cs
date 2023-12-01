d4a3d053e812cbd28e6c94338bd851e3b7a7ee50
hanyvari.mark <hanyvarimark@stud.uni-obuda.hu>
2023-12-01 10:20:26 +0100

    Rover model modified

--- DEAIC6_HFT_2023241.Models/Rover.cs
+++ DEAIC6_HFT_2023241.Models/Rover.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace DEAIC6_HFT_2023241.Models
@@ -14,30 +15,38 @@ namespace DEAIC6_HFT_2023241.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int RoverId { get; set; }
         [StringLength(60)]
+        [Required]
         public string RoverName { get; set; }
-
-        public DateTime BuildTime { get; set; }
+        //public DateTime BuildTime { get; set; }
+        [Required]
         public DateTime LaunchDate { get; set; }
+        [Required]
         public DateTime LandDate { get; set; }
-
+        [Required]
         public int VisitedPlaceId { get; set; }
+        [Required]
         public int BuilderId { get; set; }
+        [NotMapped]
+        public virtual RoverBuilder RoverBuilder { get; set; }
 
-        public virtual VisitedPlaces VisitedPlaces { get; private set; }
-        public virtual RoverBuilder RoverBuilders { get; private set; }
+        //public virtual VisitedPlaces VisitedPlaces { get; private set; }
+        //public virtual RoverBuilder RoverBuilders { get; private set; }
 
+        public Rover()
+        {
+            
+        }
 
         public Rover(string line)
         {
-            //RoverId#RoverName#LaunchDate#LandDate#VisitedPlaceId#BuilderId
+            //RoverId#RoverName#LaunchDate#LandDate#BuilderId
             //1 Active 0 not Active -1 will be active
             string[] roverline = line.Split('#');
             RoverId = int.Parse(roverline[0]);
             RoverName = roverline[1];
             LaunchDate = DateTime.Parse(roverline[2]);
             LandDate = DateTime.Parse(roverline[3]);
-            VisitedPlaceId = int.Parse(roverline[6]);
-            BuilderId = int.Parse(roverline[7]);
+            BuilderId = int.Parse(roverline[4]);
         }
     }
 }
