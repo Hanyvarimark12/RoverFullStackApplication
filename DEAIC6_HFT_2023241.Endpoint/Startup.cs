c0bcbcf743cb5aa4e64e9501303e1eb9546b5bab
hanyvari.mark <hanyvarimark@stud.uni-obuda.hu>
2023-12-01 10:26:01 +0100

    Startup modified

--- DEAIC6_HFT_2023241.Endpoint/Startup.cs
+++ DEAIC6_HFT_2023241.Endpoint/Startup.cs
@@ -1,12 +1,20 @@
+using DEAIC6_HFT_2023241.Logic;
+using DEAIC6_HFT_2023241.Models;
+using DEAIC6_HFT_2023241.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Diagnostics;
 
 namespace DEAIC6_HFT_2023241.Endpoint
 {
@@ -14,8 +22,30 @@ namespace DEAIC6_HFT_2023241.Endpoint
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<RoverDbContext>();
+
+            services.AddTransient<IRepository<Rover>, RoverRepository>();
+            services.AddTransient<IRepository<RoverBuilder>, RoverBuilderRepository>();
+            services.AddTransient<IRepository<VisitedPlaces>, VisitedPlacesRepository>();
+
+            services.AddTransient<IRoverLogic, RoverLogic>();
+            services.AddTransient<IRoverBuilderLogic, RoverBuilderLogic>();
+            services.AddTransient<IVisitedPlacesLogic, VisitedPlacesLogic>();
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "DEAIC6_HFT_2023241.Endpoint", Version = "v1" });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,15 +56,30 @@ namespace DEAIC6_HFT_2023241.Endpoint
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "DEAIC6_HFT_2023241_API");
+                c.RoutePrefix = string.Empty;
+            });
+
+            app.UseExceptionHandler(c => c.Run(async context =>
+            {
+                var ex = context.Features
+                    .Get<IExceptionHandlerPathFeature>()
+                    .Error;
+                var resp = new { Msg = ex.Message };
+                await context.Response.WriteAsJsonAsync(resp);
+            }));
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
+
+
     }
 }
