acf0233f1dab8d532489263cb8b093f6d3b1001d
hanyvari.mark <hanyvarimark@stud.uni-obuda.hu>
2023-12-01 10:18:30 +0100

    RoverBuilder modified

--- DEAIC6_HFT_2023241.Logic/RoverBuilderLogic.cs
+++ DEAIC6_HFT_2023241.Logic/RoverBuilderLogic.cs
@@ -5,23 +5,27 @@ using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel.Design;
+using System.Security.Cryptography.X509Certificates;
 
 namespace DEAIC6_HFT_2023241.Logic
 {
-    public class RoverBuilderLogic 
+    public class RoverBuilderLogic : IRoverBuilderLogic
     {
         IRepository<RoverBuilder> builder_repo;
-        IRepository<Rover> rover_repo;
+        //IRepository<Rover> rover_repo;
+        //IRepository<VisitedPlaces> visitedplaces_repo;
 
-        public RoverBuilderLogic(IRepository<RoverBuilder> builder_repo, IRepository<Rover> rover_repo)
+        public RoverBuilderLogic(IRepository<RoverBuilder> builder_repo)
         {
             this.builder_repo = builder_repo;
-            this.rover_repo = rover_repo;
+            //IRepository<Rover> rover_repo
+            //this.rover_repo = rover_repo;
+            //this.visitedplaces_repo = visitedplaces_repo;
         }
 
         public void Create(RoverBuilder element)
         {
-            if(element.BuilderId < 0 ||  element.BuilderName == null) 
+            if (element.BuilderId < 0 || element.BuilderName == null)
             {
                 throw new ArgumentException("Cannot create RoverBuilder without name!/RoverBuilderId cannot be null!");
             }
@@ -39,7 +43,7 @@ namespace DEAIC6_HFT_2023241.Logic
         public RoverBuilder Read(int id)
         {
             var builder = this.builder_repo.Read(id);
-            if(builder == null)
+            if (builder == null)
             {
                 throw new ArgumentException("RoverBuilder does not exist!");
             }
@@ -56,46 +60,80 @@ namespace DEAIC6_HFT_2023241.Logic
             this.builder_repo.Update(Element);
         }
 
-        public IEnumerable<BuilderTime> AvgTimeTravel(int BuilderId)
+        public IEnumerable<RoverBuilded> RoverBuild()
         {
-            var builderTime = rover_repo.ReadAll().Where(t => t.BuilderId == BuilderId);
-            var timetraveled = (IEnumerable<BuilderTime>)from builder in builder_repo.ReadAll()
-                               where builder.BuilderId == BuilderId
-                               group builder by builder.BuilderId into roverTimeTraveled
-                               select new BuilderTime
-                               {
-                                   Id = roverTimeTraveled.Key,
-                                   AvgTraveled = rover_repo.ReadAll().Where(t => t.BuilderId == BuilderId)
-                                   .Average(t => t.LandDate.Hour - t.LaunchDate.Hour)
-                               };
-
-            return timetraveled;
+            //builder order by distance
+            return from x in this.builder_repo.ReadAll()
+                                orderby x.VisitedPlaces.Distance descending
+                                select new RoverBuilded()
+                                {
+                                    Id = x.BuilderId,
+                                    Distance = x.VisitedPlaces.Distance
+                                };
         }
 
-        public IEnumerable<Builder> MoreVisitedPlaces()
+        public MaxRoverNumber MoreVisitedPlaces()
         {
-            //roverbuilder with more than one visited places
-            return (IEnumerable<Builder>)from builder in builder_repo.ReadAll()
-                   where builder.VisitedPlaces.Count() > 1
-                   group builder by builder.BuilderId into rover_builder
-                   select new Builder()
-                   {
-                       Id = rover_builder.Key,
-                       VisitedPlaceNumber = rover_builder.Count()
-                   };
-                
-        } 
+            //visitedplace with most builders
+            var builderOnPlanet = from x in this.builder_repo.ReadAll()
+                                  group x by x.VisitedPlaceId into g
+                                  orderby g.Count() descending
+                                  select new MaxRoverNumber()
+                                  {
+                                      Id = g.Key,
+                                      BuilderNumber = g.Count(),
+                                      Name = g.Select(t => t.VisitedPlaces.PlanetName).FirstOrDefault()
+                                  };
+
+            return builderOnPlanet.FirstOrDefault();
+        }
     }
 
-    public class Builder
+    public class RoverBuilded
     {
         public int Id { get; set; }
-        public int VisitedPlaceNumber { get; set; }
+        public double Distance { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            RoverBuilded builded = obj as RoverBuilded;
+            if (builded == null)
+                return false;
+            else
+            {
+                return this.Id == builded.Id
+                    && this.Distance == builded.Distance;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(this.Id, this.Distance);
+        }
     }
 
-    public class BuilderTime
+    public class MaxRoverNumber
     {
         public int Id { get; set; }
-        public double AvgTraveled { get; set; }
+        public string Name { get; set; }
+        public int BuilderNumber { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            MaxRoverNumber planet = obj as MaxRoverNumber;
+            if (planet == null)
+                return false;
+            else
+            {
+                return this.Id == planet.Id
+                    && this.BuilderNumber == planet.BuilderNumber
+                    && this.Name == planet.Name;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(this.Id, this.BuilderNumber, this.Name);
+        }
     }
 }
