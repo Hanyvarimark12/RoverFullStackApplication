5890bdc623a92a1f116341fbf457ccc9c65f6534
hanyvari.mark <hanyvarimark@stud.uni-obuda.hu>
2023-12-01 10:41:57 +0100

    IRoverBuilderLogic added

---
+++ DEAIC6_HFT_2023241.Logic/IRoverBuilderLogic.cs
@@ -0,0 +1,17 @@
+﻿using DEAIC6_HFT_2023241.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DEAIC6_HFT_2023241.Logic
+{
+    public interface IRoverBuilderLogic
+    {
+        IEnumerable<RoverBuilded> RoverBuild();
+        void Create(RoverBuilder element);
+        void Delete(int id);
+        MaxRoverNumber MoreVisitedPlaces();
+        RoverBuilder Read(int id);
+        IQueryable<RoverBuilder> ReadAll();
+        void Update(RoverBuilder Element);
+    }
+}
