066783edd00c3a125d4c4d12a239e1db7d7b2543
hanyvari.mark <hanyvarimark@stud.uni-obuda.hu>
2023-12-04 23:51:50 +0100

    RoverBuilder Controller added

---
+++ DEAIC6_HFT_2023241.Endpoint/Controllers/RoverBuilderController.cs
@@ -0,0 +1,51 @@
+﻿using DEAIC6_HFT_2023241.Logic;
+using DEAIC6_HFT_2023241.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+
+namespace DEAIC6_HFT_2023241.Endpoint.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RoverBuilderController : ControllerBase
+    {
+        IRoverBuilderLogic logic;
+
+        public RoverBuilderController(IRoverBuilderLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<RoverBuilder> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public RoverBuilder Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody] RoverBuilder value)
+        {
+            this.logic.Create(value);
+        }
+
+
+        [HttpPut]
+        public void Updat([FromBody] RoverBuilder value)
+        {
+            this.logic.Update(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
+    }
+}
