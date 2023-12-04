cf52d2428833723f68c3d50afd7cb14c8ebc72d5
hanyvari.mark <hanyvarimark@stud.uni-obuda.hu>
2023-12-04 23:54:25 +0100

    RoverTraveled added to models

---
+++ DEAIC6_HFT_2023241.Models/RoverNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEAIC6_HFT_2023241.Models
+{
+    public class RoverNumber
+    {
+        public int BuilderId { get; set; }
+        public int Number { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            RoverNumber distance = obj as RoverNumber;
+            if (distance == null)
+                return false;
+            else
+            {
+                return this.BuilderId == distance.BuilderId
+                    && this.Number == distance.Number;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(this.BuilderId, this.Number);
+        }
+    }
+}
