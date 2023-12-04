218b52f0b0b714e04e6443f719f02d3adce8773a
hanyvari.mark <hanyvarimark@stud.uni-obuda.hu>
2023-12-04 23:55:13 +0100

    RoverNumberByPlanet added to models

---
+++ DEAIC6_HFT_2023241.Models/RoverNumberByPlanet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEAIC6_HFT_2023241.Models
+{
+    public class RoverNumberByPlanet
+    {
+        public int Id { get; set; }
+        public int RoverNumber { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            RoverNumberByPlanet planet = obj as RoverNumberByPlanet;
+            if (planet == null)
+                return false;
+            else
+            {
+                return this.Id == planet.Id
+                    && this.RoverNumber == planet.RoverNumber;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(this.Id, this.RoverNumber);
+        }
+    }
+}
