a26e1bdcfaff5a687b3dc9ed4105446c6d662d60
hanyvari.mark <hanyvarimark@stud.uni-obuda.hu>
2023-12-04 23:55:39 +0100

    RoverBuilded added to models

---
+++ DEAIC6_HFT_2023241.Models/RoverBuilded.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEAIC6_HFT_2023241.Models
+{
+    public class RoverBuilded
+    {
+        public int Id { get; set; }
+        public double Distance { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            RoverBuilded builded = obj as RoverBuilded;
+            if (builded == null)
+                return false;
+            else
+            {
+                return this.Id == builded.Id
+                    && this.Distance == builded.Distance;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(this.Id, this.Distance);
+        }
+    }
+}
